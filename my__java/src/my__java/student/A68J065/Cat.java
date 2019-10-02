0a4e99b5d8316a57aa89328fc1ad35dfc8f93fca
A68J065 <A68J065@gmail.com>
2019-10-02 15:47:40 +0800

    add inheritance relationship

--- my__java/src/my__java/student/A68J065/Cat.java
+++ my__java/src/my__java/student/A68J065/Cat.java
@@ -1,5 +1,5 @@
 package my__java.student.A68J065;
 
-public class Cat {
+public class Cat extends Animal {
 
 }
--- my__java/src/my__java/student/A68J065/Dog.java
+++ my__java/src/my__java/student/A68J065/Dog.java
@@ -1,5 +1,5 @@
 package my__java.student.A68J065;
 
-public class Dog {
+public class Dog extends Animal {
 
 }
